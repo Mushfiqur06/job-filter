7bd7fb0783fb5518f227b1bffae4169d2a601fa6
Mushfiqur Rahman <mushfiqur06@gmail.com>
2021-09-29 22:38:02 +0600

    Add GraphQL & Typescript

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,23 @@ import App from "./module/App";
 
 import { Provider } from "react-redux";
 import store from "./store";
-
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <Provider store={store}>
       <App />
     </Provider>
-    ,
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById("root")
 );
 
--- src/module/App.js
+++ src/module/App.js
@@ -2,15 +2,32 @@ import React from "react";
 import { create } from "jss";
 import preset from "jss-preset-default";
 import { ThemeProvider } from "@mui/material/styles";
-import { StylesProvider } from '@mui/styles';
+import { StylesProvider } from "@mui/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "../customization";
 import Home from "./Home";
+import { useQuery, gql } from "@apollo/client";
 
 const jss = create();
 jss.setup(preset());
 
+const GET_ALL_JOBS = gql`
+  query GetAllJobs {
+    jobs {
+      id
+      company
+    }
+  }
+`;
+
 export default function App() {
+  const { loading, error, data } = useQuery(GET_ALL_JOBS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log("Data", data);
+
   return (
     <React.StrictMode>
       <StylesProvider jss={jss}>
